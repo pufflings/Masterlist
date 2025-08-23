c1bdcc5314d22b418ec102bdc33114dfe7ef120a
grvl <7000428+grvl@users.noreply.github.com>
2025-08-23 22:41:15 +0100

    Updated rarity classifications in CSS and JavaScript, changing 'Very Rare' to 'Super Rare' and 'Legendary' to 'Exclusive' for improved clarity and consistency in the charadex styling and configuration.

--- styles/js/config.js
+++ styles/js/config.js
@@ -97,7 +97,7 @@ charadex.loadOptions = async () => {
     // charadex.sheet.options = {
     //   designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
     //   statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
-    //   rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
+    //   rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Super Rare', 'Exclusive'],
     //   species: ['All', 'Dog', 'Cat', 'Bunny'],
     //   itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
     //   traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
